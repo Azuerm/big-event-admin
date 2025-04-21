bf6ad8fc7c32576cab511df5514e9b8f64d5d412
Azuerm <3281186790@qq.com>
2025-04-21 22:44:33 +0800

    feat: 新增内容

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { useRoute, useRouter } from 'vue-router'
+// 在vue3 compositionAPI 中
+// 1.获得路由对象 router useRouter
+//  const router = useRouter()
+// 2.获得路由参数 route useRoute
+//  const route = useRoute
+const router = useRouter()
+const route = useRoute()
+const goList = () => {
+  console.log(router, route)
+  router.push('/list')
+}
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>我是App</div>
+  <button @click="$router.push('/home')">跳首页</button>
+  <button @click="goList">跳列表页</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory
+  // createWebHashHistory
+} from 'vue-router'
+// createRouter 创建路由实例
+// 配置history模式
+// 1.history模式：createWebHistory 地址栏不带#
+// 2.hash模式：createWebHashHistory 地址栏带#
 
+console.log(import.meta.env.DEV) // true
+// vite 中的环境变量 import.meta.env.BASE_URL 就是 vite.config.js 中的 base配置项
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes: []
 })
 
 export default router
