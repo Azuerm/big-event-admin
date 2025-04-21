a1b6b983cc06db7454667aa80782503616806b9d
Azuerm <3281186790@qq.com>
2025-04-21 22:52:17 +0800

    fix: 文案

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import {
 // 配置history模式
 // 1.history模式：createWebHistory 地址栏不带#
 // 2.hash模式：createWebHashHistory 地址栏带#
+// 3. vue路由懒加载
 
 console.log(import.meta.env.DEV) // true
 // vite 中的环境变量 import.meta.env.BASE_URL 就是 vite.config.js 中的 base配置项
