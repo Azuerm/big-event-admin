f626fbf04c6b91e1045913eb3884f71080fbed27
Azuerm <3281186790@qq.com>
2025-04-23 00:18:15 +0800

    update：项目目录完善

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,24 @@ const goList = () => {
   console.log(router, route)
   router.push('/list')
 }
+// 导入数据库的数据
+import { useUserStore, useCountStore } from '@/stores'
+const userStore = useUserStore()
+const countStore = useCountStore()
 </script>
 
 <template>
   <div>我是App</div>
-  <button @click="$router.push('/home')">跳首页</button>
-  <button @click="goList">跳列表页</button>
+  <test-demo></test-demo>
+  <el-button @click="$router.push('/home')">跳首页</el-button>
+  <el-button @click="goList">跳列表页</el-button>
+  <hr />
+  <p>{{ userStore.token }}</p>
+  <el-button @click="userStore.setToken('hhhhhhhh')">登录</el-button>
+  <el-button @click="userStore.removeToken()">退出</el-button>
+  <hr />
+  {{ countStore.count }}
+  <el-button @click="countStore.add(2)">加法</el-button>
 </template>
 
 <style scoped></style>
