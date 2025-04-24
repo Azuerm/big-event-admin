f253e8e3f7a98bf2fe36842f9d4b0b970e7e03d2
Azuerm <3281186790@qq.com>
2025-04-24 17:05:46 +0800

    update：main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores/index'
+// 导入基本样式
 import '@/assets/main.scss'
 const app = createApp(App)
 
